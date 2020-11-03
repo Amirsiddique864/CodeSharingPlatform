5a63f0b321fb979bce394afbc0be686e8910717f
Amir Siddique <amirsiddique864@gmail.com>
2020-11-02 22:54:40 -0800

    final commit

--- frontend/codesharing/src/App.js
+++ frontend/codesharing/src/App.js
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+
 import './App.css';
 import TextEditor from './Containers/textEditior';
+import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
+import Landing from './Containers/Landing/Landing';
 
-function App() {
-  return (
-    <div className="App">
-        <TextEditor/>
-    </div>
-  );
+class App extends Component {
+  state = {
+    p: null
+  }
+  componentDidMount() {
+    // axios.get('http://localhost:3005/').then(res => {
+    //   console.log(res.data);
+    //   this.setState({ p: "/" + res.data.roomName });
+    // })
+  }
+
+
+  render() {
+    let routes = (
+      <Switch>
+          <Route path="/" exact component={Landing} />
+          <Redirect to="/" />
+        </Switch>
+    );
+    if(this.props.isAuth){
+      routes = (
+        <Switch>
+          <Route path="/editor" component={TextEditor} />
+          <Route path="/" exact component={Landing} />
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
+    
+    return (  
+      <div className="App">
+        {routes}
+      </div>
+    );
+  }
+
 }
 
-export default App;
+const mapStateToProps = (state)=>{
+  return{
+    isAuth: state.auth.isAuth
+  }
+}
+
+export default connect(mapStateToProps,null)(withRouter(App));
